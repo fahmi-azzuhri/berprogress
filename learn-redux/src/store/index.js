115d138da6c7822821ac5471696a93b5f4edcd6f
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-02-14 07:47:29 +0700

    add learn-redux

---
+++ learn-redux/src/store/index.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "./slices/counterSlices";
+export const store = configureStore({
+  reducer: {
+    counterSlice: counterSlice.reducer,
+  },
+});
---
+++ learn-redux/src/store/slices/counterSlices.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  counter: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counterSlice",
+  initialState,
+  reducers: {},
+});
+
+console.log(counterSlice);
