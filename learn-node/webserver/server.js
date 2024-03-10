3881f927aa8e1eb08aaa95015ba3e7c7acfcb14c
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-03-10 20:41:42 +0700

    http server

---
+++ learn-node/webserver/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = 3000;
+const host = "127.0.0.1";
+
+const requestListener = (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+  response.statusCode = 200;
+  response.end("Hello World!");
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
