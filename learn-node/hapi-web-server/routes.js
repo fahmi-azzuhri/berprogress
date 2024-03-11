77da12eb7e2d5f585620742d91b598fa7f66e0f3
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-03-11 22:47:40 +0700

    hapi query parameter

--- learn-node/hapi-web-server/routes.js
+++ learn-node/hapi-web-server/routes.js
@@ -18,7 +18,10 @@ const routes = [
     path: "/hello/{name?}",
     handler: (request, h) => {
       const { name = "stranger" } = request.params;
-      return `Hello, ${name}`;
+      const { lang } = request.query;
+      if (lang === "id") {
+        return `Hai, ${name}!`;
+      }
     },
   },
 ];
