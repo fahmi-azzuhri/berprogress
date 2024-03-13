287fd2ed405336c97d1be3021168ad0080f34213
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-03-13 21:20:06 +0700

    hapi body payload

--- learn-node/hapi-web-server/routes.js
+++ learn-node/hapi-web-server/routes.js
@@ -24,6 +24,14 @@ const routes = [
       }
     },
   },
+  {
+    method: "POST",
+    path: "/login",
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      return `Welcome ${username}`;
+    },
+  },
 ];
 
 module.exports = routes;
