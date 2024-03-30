c715d8b9524fa2c1a206d0e4e1b4eea5e15787e5
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-03-30 23:29:00 +0700

    learn any ts

---
+++ learn-ts/tests/any.test.ts
@@ -0,0 +1,12 @@
+describe("any", () => {
+  it("any", () => {
+    const person: any = {
+      name: "fahmi",
+      age: 44,
+      address: "bekasi",
+      isMarriage: false,
+    };
+
+    console.log(person);
+  });
+});
