2fcb9e809209972ed3ddf176d05430601c55036f
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-05-12 13:14:35 +0700

    string dart

---
+++ learn-dart/bin/string.dart
@@ -0,0 +1,14 @@
+void main() {
+  var firstName = 'Fahmi';
+  var lastName = "Azzuhri Efki";
+
+  print(firstName + lastName);
+  print("${firstName} $lastName"); //string interpolation
+
+  String longText = """
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. """; //multiline text
+
+  print(longText);
+}
