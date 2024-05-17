8ddd8e1ec8875f641820ee15024ff058e99eac7b
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-05-17 22:11:11 +0700

    for in dart

---
+++ learn-dart/bin/forin.dart
@@ -0,0 +1,10 @@
+void main() {
+  var names = ['Fahmi', 'Azzuhri', 'Efki'];
+  // for (var i = 0; i < names.length; i++) {
+  //   print(names[i]);
+  // }
+
+  for (String name in names) {
+    print(name);
+  }
+}
