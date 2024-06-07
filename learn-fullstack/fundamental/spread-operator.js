0230b58e9028824e87d7231a65a4808938c6c86a
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-06-07 23:39:47 +0700

    spread operator

---
+++ learn-fullstack/fundamental/spread-operator.js
@@ -0,0 +1,11 @@
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [...array1];
+
+console.log("array 1", array1);
+console.log("array 2", array2);
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6, 7];
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
