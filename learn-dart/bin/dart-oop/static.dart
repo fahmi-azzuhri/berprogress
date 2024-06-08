366ad6c9c2e88663ac3d04659c12e6845c684790
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-06-08 23:52:47 +0700

    static dart

---
+++ learn-dart/bin/dart-oop/static.dart
@@ -0,0 +1,9 @@
+class Player {
+  static final name = "Fahmi";
+  static final country = "Indonesia";
+}
+
+void main() {
+  print(Player.name); //manggil nya nama class langsung ke field nya
+  print(Player.country);
+}
