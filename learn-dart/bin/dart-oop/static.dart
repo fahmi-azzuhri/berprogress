456344447f7df5c249d45c8b6308a59ed5d6c48e
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-06-08 23:56:54 +0700

    static dart

--- learn-dart/bin/dart-oop/static.dart
+++ learn-dart/bin/dart-oop/static.dart
@@ -3,7 +3,14 @@ class Player {
   static final country = "Indonesia";
 }
 
+class Math {
+  static int sum(int first, int second) {
+    return first + second;
+  }
+}
+
 void main() {
   print(Player.name); //manggil nya nama class langsung ke field nya
   print(Player.country);
+  print(Math.sum(9, 9));
 }
