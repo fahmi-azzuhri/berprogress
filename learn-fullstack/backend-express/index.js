8cac94009001ceb762dd0aec463f5acc209d9cbc
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-06-09 01:30:32 +0700

    konfigurasi CORS dan Body Parser

--- learn-fullstack/backend-express/index.js
+++ learn-fullstack/backend-express/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+
 const port = 9000;
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
