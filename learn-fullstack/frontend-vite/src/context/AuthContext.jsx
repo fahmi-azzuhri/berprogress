f0129a40f1575c15e1a4476fd0ed7491475580e9
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-06-20 21:10:07 +0700

    create auth context

---
+++ learn-fullstack/frontend-vite/src/context/AuthContext.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, createContext } from "react";
+import Cookies from "js-cookie";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!Cookies.get("token")
+  );
+
+  useEffect(() => {
+    const handleTokenChange = () => {
+      setIsAuthenticated(!!Cookies.get("token"));
+
+      window.addEventListener("storage", handleTokenChange);
+      return () => {
+        window.removeEventListener("storage", handleTokenChange);
+      };
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }} />
+  );
+};
